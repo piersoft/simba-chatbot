a3fc46a81721f1b18f09c4aca9dfee840bf9ad1e
Francesco Piero Paolicelli <piersoft2@gmail.com>
2026-03-10 22:35:15 +0100

    Update App.jsx

--- ckan-chat/frontend/src/App.jsx
+++ ckan-chat/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import ChatMessage from "./components/ChatMessage";
 import ToolCallBadge from "./components/ToolCallBadge";
 import StatusBar from "./components/StatusBar";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
+const BACKEND_URL = "";
 
 const SUGGESTIONS = [
   "Cerca dataset sulla qualità dell'aria",
